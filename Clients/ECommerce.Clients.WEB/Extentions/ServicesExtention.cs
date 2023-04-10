e379d308b3c3f1789122bb1bdf9d29b322731008
Yiğit Can Pekgüzel <yigitcanpekguzel@gmail.com>
2023-04-10 11:49:09 +0300

    Httpclients extention

---
+++ Clients/ECommerce.Clients.WEB/Extentions/ServicesExtention.cs
@@ -0,0 +1,52 @@
+﻿using ECommerce.Clients.WEB.Handler;
+using ECommerce.Clients.WEB.Handlers;
+using ECommerce.Clients.WEB.Models;
+using ECommerce.Clients.WEB.Services.GeneralServices;
+using ECommerce.Clients.WEB.Services.IdentityServices;
+using ECommerce.Clients.WEB.Services.Interfaces;
+using ECommerce.Clients.WEB.Services.UserServices;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace ECommerce.Clients.WEB.Extentions
+{
+    public static class ServicesExtention
+    {
+        public static void AddHttpClientServices(this IServiceCollection services,WebApplicationBuilder builder)
+        {
+           
+           
+            ServiceApiSettings serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
+
+            services.AddHttpClient<IClientCredentialTokenService, ClientCredentialService>();
+            services.AddHttpClient<IIdentityService, IdentityServices>();
+            services.AddHttpClient<IBasketService, BasketService>(opt =>
+            {
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GateWayUrl}/{serviceApiSettings.Basket.path}");
+            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+            services.AddHttpClient<ICatalogService, CatalogService>(opt =>
+            {
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GateWayUrl}/{serviceApiSettings.Catalog.path}");
+            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+
+            services.AddHttpClient<IPhotoStockService, PhotoStockService>(opt =>
+            {
+                opt.BaseAddress = new Uri($"{serviceApiSettings.GateWayUrl}/{serviceApiSettings.PhotoStock.path}");
+            }).AddHttpMessageHandler<ClientCredentialTokenHandler>();
+
+            services.AddHttpClient<IUserService, UserService>(opt =>
+            {
+                opt.BaseAddress = new Uri(builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>().IdentityUrl);
+            }).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opt =>
+            {
+                opt.LoginPath = "/Auth/SignIn";
+                opt.ExpireTimeSpan = TimeSpan.FromDays(60);
+                opt.SlidingExpiration = true;
+                opt.Cookie.Name = "ecoomercewebcookie";
+            });
+
+        }
+    }
+}
--- Clients/ECommerce.Clients.WEB/Program.cs
+++ Clients/ECommerce.Clients.WEB/Program.cs
@@ -1,3 +1,4 @@
+using ECommerce.Clients.WEB.Extentions;
 using ECommerce.Clients.WEB.Handler;
 using ECommerce.Clients.WEB.Handlers;
 using ECommerce.Clients.WEB.Helpers;
@@ -20,7 +21,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.Configure<ServiceApiSettings>(builder.Configuration.GetSection("ServiceApiSettings")); //options pattern
 builder.Services.Configure<ClientSettings>(builder.Configuration.GetSection("ClientSettings"));
 
-ServiceApiSettings serviceApiSettings = builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>();
+
 
 builder.Services.AddSingleton<PhotoHelper>();
 
@@ -30,31 +31,8 @@ builder.Services.AddScoped<ClientCredentialTokenHandler>();
 builder.Services.AddAccessTokenManagement();
 
 builder.Services.AddHttpContextAccessor();
-builder.Services.AddHttpClient<IClientCredentialTokenService, ClientCredentialService>();
-builder.Services.AddHttpClient<IIdentityService, IdentityServices>();
-builder.Services.AddHttpClient<ICatalogService, CatalogService>(opt =>
-{
-    opt.BaseAddress = new Uri($"{serviceApiSettings.GateWayUrl}/{serviceApiSettings.Catalog.path}");
-}).AddHttpMessageHandler<ClientCredentialTokenHandler>();
 
-builder.Services.AddHttpClient<IPhotoStockService, PhotoStockService>(opt =>
-{
-    opt.BaseAddress = new Uri($"{serviceApiSettings.GateWayUrl}/{serviceApiSettings.PhotoStock.path}");
-}).AddHttpMessageHandler<ClientCredentialTokenHandler>();
-
-builder.Services.AddHttpClient<IUserService, UserService>(opt =>
-{
-    opt.BaseAddress = new Uri(builder.Configuration.GetSection("ServiceApiSettings").Get<ServiceApiSettings>().IdentityUrl);
-}).AddHttpMessageHandler<ResourceOwnerPasswordTokenHandler>();
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, opt =>
-{
-    opt.LoginPath = "/Auth/SignIn";
-    opt.ExpireTimeSpan = TimeSpan.FromDays(60);
-    opt.SlidingExpiration = true;
-    opt.Cookie.Name = "ecoomercewebcookie";
-}) ;
-
+builder.Services.AddHttpClientServices(builder);
 
 var app = builder.Build();
 
